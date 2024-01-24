1e7f62baab22126e9781840f9d98fa026e7fd9c0
lyfsn <dev.wangyu@proton.me>
2024-01-24 16:29:06 +0800

    up

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-docker build -t endurance/nethermind .
+docker build --build-arg BUILDPLATFORM=linux/amd64 --build-arg TARGETARCH=amd64 --build-arg TARGETPLATFORM=linux/amd64 -t endurance/nethermind .
